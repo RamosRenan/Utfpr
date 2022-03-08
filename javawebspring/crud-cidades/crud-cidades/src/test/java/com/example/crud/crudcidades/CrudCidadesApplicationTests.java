5eaa0d1fc6dec72d69eb593403f6dcb6b368a052
RamosRenan <renan.barbosards@gmail.com>
2022-03-07 23:58:25 -0300

    starting project crud cidades

---
+++ javawebspring/crud-cidades/crud-cidades/src/test/java/com/example/crud/crudcidades/CrudCidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud.crudcidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
